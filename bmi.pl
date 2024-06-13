6ca6cf579784d92573f56723cdc180c051f30e3f
OjciecPablito <34928406+OjciecPablito@users.noreply.github.com>
2024-06-14 00:12:16 +0200

    first element of bmi file

---
+++ bmi.pl
@@ -0,0 +1,18 @@
+/*
+dane są fakty opisane predykatem osoba(imię, wzrost, waga)
+Stosując wzór na obliczenie wzkaźnika bmi (mody mass of index)
+Postać wzoru:
+    Bmi = waga/(wzrost/100)^2
+
+Zadanie:
+Napisz reguły bmi(X, Y).
+gdzie:  X - dana osoba
+        Y - wynik na co cierpi osoba
+
+Przyjęta ocena wkaźnika:
+    bmi < 20        niedowaga
+    20 <= bmi < 25  zdrowy
+    25 <= bmi < 35  nadwaga
+    35 <= bmi < 40  otyłość
+    40 <= bmi       chorobliwa otyłość
+*/
